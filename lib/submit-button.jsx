e4c923394eeff348d412defdcf65b0f0d13ab8a1
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-12 12:07:04 -0600

    submit button appears on DOM

---
+++ lib/submit-button.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+class ActionButton extends React.Component {
+  render () {
+    return (
+        <button className="ActionButton">
+          <span>Submit</span>
+        </button>
+    )
+  }
+}
+
+ReactDOM.render(<ActionButton  />, document.getElementById('submit'))
+
+module.exports = ActionButton
