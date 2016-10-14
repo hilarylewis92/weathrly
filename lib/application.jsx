878d8f845d191de3d4eabccb41b7dd8126971cac
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-13 21:55:28 -0600

    weathrly app rendering into h1

--- lib/application.jsx
+++ lib/application.jsx
@@ -4,39 +4,44 @@ const ReactDOM = require('react-dom');
 
 
 class Application extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {location: 'Location'};
+  constructor() {
+    super();
+    this.state = {
+      location: '',
+      temperature: ''
+    };
   }
+
   searchData(e){
     e.preventDefault();
     $.get(this.props.source + this.state.location, function(data){
-      console.log(data);
-      // this.setState({
-      //   // location: result
+      this.setState({
+        location: '',
+        temperature: ''
       });
-    // }.bind(this));
+    });
   }
 
   render(){
     return (
       <section>
         <article id="input-form">
-          <h1 className="title"> Weathrly </h1>
-            <input className="input-field" placeholder="Enter Location" value={this.state.location} onChange={(e)=>this.setState({location: e.target.value})}/>
-            <button id="submit-btn" onClick={(e) => this.searchData(e)}> Submit </button>
+          <h1 className="title"> {this.props.title} </h1>
+          <input className="input-field" placeholder="Enter Location" value={this.state.location} onChange={(e)=>this.setState({location: e.target.value})}/>
+          <button id="submit-btn" onClick={(e) => this.searchData(e)}> Submit </button>
         </article>
+
         <article id="weather-display">
           <span id="city"> {this.state.location} </span>
           <span id="temperature"> Temp </span>
           <span id="weather"> Weather Type </span>
-            <img src="#"/>
+          <img src="#"/>
         </article>
       </section>
-    )
+    );
   }
 }
 
-ReactDOM.render(<Application source='https://weatherly-api.herokuapp.com/api/weather/'/>, document.getElementById('app'));
+ReactDOM.render(<Application title='Weathrly App' source='https://weatherly-api.herokuapp.com/api/weather/'/>, document.getElementById('app'));
 
 module.exports = Application
