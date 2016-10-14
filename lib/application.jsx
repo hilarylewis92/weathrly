8442cc6a43920333cc37bddd70b54863ace8662b
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-13 21:42:23 -0600

    refactored css call in index.js

--- lib/application.jsx
+++ lib/application.jsx
@@ -1,7 +1,7 @@
 const $ = require('jquery');
 const React = require('react');
 const ReactDOM = require('react-dom');
-require('./styles');
+
 
 class Application extends React.Component {
   constructor(props) {
