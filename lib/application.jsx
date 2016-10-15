032063e394b4cafb5ae23d0db925357e736dabe3
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-10-14 20:44:33 -0600

    functionality

--- lib/application.jsx
+++ lib/application.jsx
@@ -8,12 +8,17 @@ class Application extends React.Component {
     super();
     this.state = {
       weather: [],
+      location: ''
     };
   }
 
+  updateInputValueInState(e){
+      this.setState({location: e.target.value});
+   }
+
   getAPIData(e){
     e.preventDefault();
-    $.get(this.props.source, function(data){
+    $.get(this.props.source + this.state.location, function(data){
       console.log(data);
       this.setState({
         weather: data,
@@ -32,7 +37,7 @@ class Application extends React.Component {
       <section>
         <article id="input-form">
           <h1 className="title"> {this.props.title} </h1>
-          <input className="input-field" type='text' placeholder="Enter Location"/>
+          <input className="input-field" type='text' placeholder="Enter Location" value={this.state.location} onChange={(e)=>this.setState({location: e.target.value})}/>
           <input id="submit-btn" type='submit' onClick={this.getAPIData.bind(this)} />
         </article>
         <WeatherDisplay weekInfo={this.state.weather} temperatureHigh={this.state.temphigh} temperatureLow={this.state.templow} locationInfo={this.state.location} weatherType={this.state.weathertype} weatherChance={this.state.chance}/>
@@ -56,6 +61,6 @@ class WeatherDisplay extends React.Component {
 }
 }
 
-ReactDOM.render(<Application title='Weathrly App' source='https://weatherly-api.herokuapp.com/api/weather/denver'/>, document.getElementById('app'));
+ReactDOM.render(<Application title='Weathrly App' source='https://weatherly-api.herokuapp.com/api/weather/'/>, document.getElementById('app'));
 
 module.exports = Application
