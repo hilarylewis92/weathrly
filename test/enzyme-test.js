cfd1b3cbccd9c2c44bff0089595b9ba60cef975a
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-10-17 17:23:19 -0600

    some tests passing

--- test/enzyme-test.js
+++ test/enzyme-test.js
@@ -1,5 +1,33 @@
 var expect = require('chai').expect;
 const React = require('react');
 const Application = require('../lib/application');
+// const Application = require('../lib/application');
 // require('locus');
 import {shallow, mount, render} from 'enzyme';
+
+
+  it('should render to the page', function() {
+    const browser = shallow(<Application/>);
+    expect(browser.find('.input-field')).to.have.length(1);
+});
+
+it('should render to the page', function() {
+  const browser = shallow(<Application/>);
+  expect(browser.find('#submit-btn')).to.have.length(1);
+});
+
+it('should allow me to input a location', function(){
+  const wrapper = mount(<Application/>);
+  // const input = wrapper.find('.input-field');
+  // const submit = wrapper.find('#submit-btn');
+  // input.simulate('change', {target: {value:'castle-rock'} });
+  // submit.simulate('click');
+  expect(wrapper.state('location').to.equal(''));
+});
+
+it('should allow me to click submit', function(){
+  const wrapper = mount(<Application/>);
+  const submit = wrapper.find('#submit-btn');
+  submit.simulate('click');
+  expect(wrapper.state('weather').to.equal('data'));
+});
