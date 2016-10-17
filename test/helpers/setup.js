0ff4320ecc30d91d3f8a106d6fc285e0c957ac30
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-10-17 16:30:00 -0600

    test setup

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -4,9 +4,18 @@ require('babel-register')({
 require('babel-polyfill');
 
 global.document = require('jsdom').jsdom(
-  "<head> <meta charset='UTF-8'><title>React In Theory</title></head><body><div id='application'></div>"
+  "<head> <meta charset='UTF-8'><title>React In Theory</title></head><body><div id='app'></div></body>"
 );
 
 global.window = document.defaultView;
 
 global.navigator = window.navigator;
+
+if (!global.window.localStorage) {
+  localStorage = {
+    getItem() { return '{}'; },}
+}
+
+if (typeof(exports) !== "undefined"){
+  var $ = require('jquery');
+}
